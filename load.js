4cbd73df84e6e3d15399e2365b20c6ab30f87566
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-06-06 16:16:00 +0300

    better structure according to the tutorial, but with high scores

---
+++ load.js
@@ -0,0 +1,14 @@
+var load_state = {
+	    preload: function() { 
+		this.game.stage.backgroundColor = '#71c5cf';
+
+    	// Load the bird sprite
+    	this.game.load.image('bird', 'assets/bird.png');
+    	this.game.load.image('pipe', 'assets/pipe.png');
+    },
+    
+    create: function() {
+        // When all assets are loaded, go to the 'menu' state
+        this.game.state.start('menu');
+    }
+};
