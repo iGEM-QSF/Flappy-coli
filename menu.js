55b09c9b7fc25f05ed61b12b27c14604b57dd256
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-06-19 14:10:52 +0300

    tap for start screen

--- menu.js
+++ menu.js
@@ -3,6 +3,7 @@ var menu_state = {
         // Call the 'start' function when pressing the spacebar
         var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         space_key.onDown.add(this.start, this); 
+        game.input.onTap.add(this.jump, this);
 
         // Defining variables
         var style = { font: "30px Arial", fill: "#ffffff" };
